09cf2a80f95abbc58b0acc8830db4b4d7af249ca
Anatol Belski <ab@php.net>
2012-07-28 19:09:25 +0000

    missing file )
    git-svn-id: http://svn.php.net/repository/pecl/trader/trunk@326853 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ functions/trader_utility.c
@@ -0,0 +1,62 @@
+/*
+	Copyright (c) 2012, Anatoliy Belsky <ab@php.net>
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions
+	are met:
+
+	- Redistributions of source code must retain the above copyright
+	notice, this list of conditions and the following disclaimer.
+
+	- Redistributions in binary form must reproduce the above copyright
+	notice, this list of conditions and the following disclaimer in the
+	documentation and/or other materials provided with the distribution.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+	A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+	HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+	SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+	LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+	DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+	THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/* $Id$ */
+
+#include "php.h"
+#include "php_trader.h"
+
+#include <ta_func.h>
+#include <ta_common.h>
+
+ZEND_EXTERN_MODULE_GLOBALS(trader)
+
+/* {{{ proto void trader_set_unstable_period(int functionId, int timePeriod)
+ see more here http://www.ta-lib.org/d_api/ta_setunstableperiod.html */
+PHP_FUNCTION(trader_set_unstable_period)
+{
+}
+/* }}} */
+
+/* {{{ proto void trader_get_unstable_period(int functionId) */
+PHP_FUNCTION(trader_get_unstable_period)
+{
+}
+/* }}} */
+
+/* {{{ proto void trader_set_compat(compatId) */
+PHP_FUNCTION(trader_set_compat)
+{
+}
+/* }}} */
+
+/* {{{ proto void trader_get_compat(void)*/
+PHP_FUNCTION(trader_get_compat)
+{
+}
+/* }}} */
