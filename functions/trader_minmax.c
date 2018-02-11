5a0e1e8b46789f78113b845fb304d4821605b3c3
Anatol Belski <ab@php.net>
2018-02-11 17:37:17 +0000

    Update copyright
    git-svn-id: http://svn.php.net/repository/pecl/trader/trunk@344222 c90b9560-bf6c-de11-be94-00142212c4b1

--- functions/trader_minmax.c
+++ functions/trader_minmax.c
@@ -1,5 +1,5 @@
 /*
-	Copyright (c) 2012, Anatoliy Belsky <ab@php.net>
+	Copyright (c) 2012-2018, Anatol Belski <ab@php.net>
 	All rights reserved.
 
 	Redistribution and use in source and binary forms, with or without
